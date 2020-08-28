2bffd9a1bd0312bd6ee2f4b59adf777641fad4ae
Victor Yevitchenko <victor.yevitchenko@motorsport.com>
2020-08-29 00:55:13 +0500

    Removed Settings from ToolBar.

--- app/src/main/java/com/victorsmind/launchonwakeup/MainActivity.kt
+++ app/src/main/java/com/victorsmind/launchonwakeup/MainActivity.kt
@@ -6,7 +6,6 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -74,16 +73,6 @@ class MainActivity : AppCompatActivity() {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 
     fun sendMessage(view: View) {
